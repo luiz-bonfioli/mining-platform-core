077e9e5d88ea755dcc446ba84dca479835a5188e
Luiz Bonfioli <luiz.bonfioli@gmail.com>
2020-08-12 14:02:51 -0300

    Initial commit

---
+++ src/main/kotlin/com/mining/platform/core/MiningPlatformCoreApplication.kt
@@ -0,0 +1,11 @@
+package com.mining.platform.core
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MiningPlatformCoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<MiningPlatformCoreApplication>(*args)
+}
