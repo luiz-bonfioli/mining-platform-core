1955d037ce902129f8bdf131ab9cd6ad429fb10a
Luiz Bonfioli <luiz.bonfioli@gmail.com>
2020-10-30 10:11:22 -0300

    Order and operation implementation

--- src/main/kotlin/com/mining/platform/core/converter/ValueObjectConverter.kt
+++ src/main/kotlin/com/mining/platform/core/converter/ValueObjectConverter.kt
@@ -2,7 +2,6 @@ package com.mining.platform.core.converter
 
 import com.mining.platform.core.controller.ValueObject
 import com.mining.platform.core.datasource.EntityBase
-import java.util.logging.Logger
 import kotlin.reflect.KClass
 import kotlin.reflect.full.createInstance
 
@@ -10,8 +9,6 @@ import kotlin.reflect.full.createInstance
  * @author luiz.bonfioli
  */
 object ValueObjectConverter {
-
-    private val logger = Logger.getLogger(ValueObjectConverter::class.qualifiedName)
 
     fun <VO : ValueObject<E>, E : EntityBase> convert(entity: E, classVO: KClass<VO>): VO =
             classVO.createInstance().apply {
